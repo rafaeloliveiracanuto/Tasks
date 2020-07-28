853c59e7a20e3d64661602a65dd704b9c34c10bc
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-28 14:35:41 -0300

    Add getRetrofitInstance and createService functions

--- app/src/main/java/com/example/tasks/service/repository/RetrofitClient.kt
+++ app/src/main/java/com/example/tasks/service/repository/RetrofitClient.kt
@@ -1,4 +1,33 @@
 package com.example.tasks.service.repository
 
-class RetrofitClient {
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class RetrofitClient private constructor(){
+
+    companion object {
+
+        private lateinit var retrofit: Retrofit
+        private const val BASE_URL = "http://devmasterteam.com/CursoAndroidAPI/"
+
+        private fun getRetrofitInstance() : Retrofit {
+
+            val httpClient = OkHttpClient.Builder()
+            if (!::retrofit.isInitialized) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(httpClient.build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+
+            return retrofit
+        }
+
+        fun <S> createService(serviceClass: Class<S>): S {
+            return getRetrofitInstance().create(serviceClass)
+        }
+    }
 }
