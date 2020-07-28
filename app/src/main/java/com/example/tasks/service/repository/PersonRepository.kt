aff9d0a2f853836c34d7dc37528aac9432223d78
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-28 15:15:37 -0300

    Add login function to PersonRepository

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,4 +1,29 @@
 package com.example.tasks.service.repository
 
+import com.example.tasks.service.model.HeaderModel
+import com.example.tasks.service.repository.remote.PersonService
+import com.example.tasks.service.repository.remote.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class PersonRepository {
+
+    private val mRemote = RetrofitClient.createService(PersonService::class.java)
+
+    fun login(email: String, password: String) {
+        val call: Call<HeaderModel> = mRemote.login(email, password)
+        call.enqueue(object : Callback<HeaderModel> {
+            override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
+                val s = ""
+            }
+
+            override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
+                val header = response.body()
+            }
+
+        })
+
+    }
+
 }
