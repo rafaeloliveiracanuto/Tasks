827bdb3af7782e5dc22bc854bcf795220fe731df
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-28 21:19:40 -0300

    Implement APIListener members

--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,6 +2,8 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.tasks.service.listener.APIListener
+import com.example.tasks.service.model.HeaderModel
 import com.example.tasks.service.repository.PersonRepository
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
@@ -11,7 +13,16 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
      * Faz login usando API
      */
     fun doLogin(email: String, password: String) {
-        mPersonRepository.login(email, password)
+        mPersonRepository.login(email, password, object : APIListener {
+            override fun onSuccess(model: HeaderModel) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(str: String) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
     /**
