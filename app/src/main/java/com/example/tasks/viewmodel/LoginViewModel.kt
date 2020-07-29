97ad4e1bb2d119452b6884e941bcee592b3d03ce
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-28 21:28:46 -0300

    Add LiveData

--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,6 +2,8 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.model.HeaderModel
 import com.example.tasks.service.repository.PersonRepository
@@ -9,25 +11,23 @@ import com.example.tasks.service.repository.PersonRepository
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val mPersonRepository = PersonRepository()
-    /**
-     * Faz login usando API
-     */
+
+    private val mLogin = MutableLiveData<Boolean>()
+    var login: LiveData<Boolean> = mLogin
+
     fun doLogin(email: String, password: String) {
         mPersonRepository.login(email, password, object : APIListener {
             override fun onSuccess(model: HeaderModel) {
-                TODO("Not yet implemented")
+                mLogin.value = true
             }
 
             override fun onFailure(str: String) {
-                TODO("Not yet implemented")
+                mLogin.value = false
             }
 
         })
     }
 
-    /**
-     * Verifica se usuário está logado
-     */
     fun verifyLoggedUser() {
     }
 
