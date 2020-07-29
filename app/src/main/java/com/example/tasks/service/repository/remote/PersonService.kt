9edf05bef3e6bd548eab8ab117a746dbb59d9736
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-29 18:15:42 -0300

    Create and implement create function in PersonService

--- app/src/main/java/com/example/tasks/service/repository/remote/PersonService.kt
+++ app/src/main/java/com/example/tasks/service/repository/remote/PersonService.kt
@@ -15,4 +15,12 @@ interface PersonService {
         @Field("password") password: String
     ): Call<HeaderModel>
 
+    @POST("Authentication/Create")
+    @FormUrlEncoded
+    fun create(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<HeaderModel>
+
 }
