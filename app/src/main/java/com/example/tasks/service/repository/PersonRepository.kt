308205b578a38a5b4745a975d2bcf9a26a6bad54
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-29 15:36:33 -0300

    Add context to the class

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,5 +1,7 @@
 package com.example.tasks.service.repository
 
+import android.content.Context
+import com.example.tasks.R
 import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.model.HeaderModel
@@ -10,7 +12,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PersonRepository {
+class PersonRepository(val context: Context){
 
     private val mRemote = RetrofitClient.createService(PersonService::class.java)
 
@@ -18,7 +20,7 @@ class PersonRepository {
         val call: Call<HeaderModel> = mRemote.login(email, password)
         call.enqueue(object : Callback<HeaderModel> {
             override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
-                listener.onFailure(t.message.toString())
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
             }
 
             override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
