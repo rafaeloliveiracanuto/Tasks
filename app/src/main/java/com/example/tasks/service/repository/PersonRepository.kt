b520201e4b89f762c9edd7fe4984f2f0f941bbc6
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-28 21:43:46 -0300

    Add validation to errors from API response

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,9 +1,11 @@
 package com.example.tasks.service.repository
 
+import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.model.HeaderModel
 import com.example.tasks.service.repository.remote.PersonService
 import com.example.tasks.service.repository.remote.RetrofitClient
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,7 +22,12 @@ class PersonRepository {
             }
 
             override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
-                response.body()?.let { listener.onSuccess(it) }
+                if (response.code() != TaskConstants.HTTP.SUCCESS) {
+                    val validation = Gson().fromJson(response.errorBody()!!.string(), String::class.java)
+                    listener.onFailure(validation)
+                } else {
+                    response.body()?.let { listener.onSuccess(it) }
+                }
             }
 
         })
