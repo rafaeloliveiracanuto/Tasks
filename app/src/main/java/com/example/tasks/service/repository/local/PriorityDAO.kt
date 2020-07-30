2b8780af04543250485adec655b8bc72c1b4887a
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-30 15:54:29 -0300

    Add clear function

--- app/src/main/java/com/example/tasks/service/repository/local/PriorityDAO.kt
+++ app/src/main/java/com/example/tasks/service/repository/local/PriorityDAO.kt
@@ -2,6 +2,7 @@ package com.example.tasks.service.repository.local
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.tasks.service.model.PriorityModel
 
 @Dao
@@ -9,4 +10,7 @@ interface PriorityDAO {
 
     @Insert
     fun save(list: List<PriorityModel>)
+
+    @Query("DELETE FROM priority")
+    fun clear()
 }
