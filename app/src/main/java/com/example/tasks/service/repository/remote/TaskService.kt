a54f470686072cbdb9542b2fd724313e194bbc4b
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-29 23:10:07 -0300

    Add GET functions

--- app/src/main/java/com/example/tasks/service/repository/remote/TaskService.kt
+++ app/src/main/java/com/example/tasks/service/repository/remote/TaskService.kt
@@ -1,5 +1,21 @@
 package com.example.tasks.service.repository.remote
 
+import com.example.tasks.service.model.TaskModel
+import retrofit2.Call
+import retrofit2.http.*
+
 interface TaskService {
+    @GET("Task")
+    fun all(): Call<List<TaskModel>>
+
+    @GET("Task/Next7Days")
+    fun nextWeek(): Call<List<TaskModel>>
+
+    @GET("Task/Overdue")
+    fun overdue(): Call<List<TaskModel>>
+
+    @GET("Task/{id}")
+    fun load(@Path(value = "id", encoded = true) id: Int): Call<TaskModel>
+
     
 }
