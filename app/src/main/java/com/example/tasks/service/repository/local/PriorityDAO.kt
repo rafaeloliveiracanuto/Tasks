e8122436effffa4602a641d9e01816f6b629e641
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-30 15:36:46 -0300

    Add save function

--- app/src/main/java/com/example/tasks/service/repository/local/PriorityDAO.kt
+++ app/src/main/java/com/example/tasks/service/repository/local/PriorityDAO.kt
@@ -1,5 +1,12 @@
 package com.example.tasks.service.repository.local
 
+import androidx.room.Dao
+import androidx.room.Insert
+import com.example.tasks.service.model.PriorityModel
+
+@Dao
 interface PriorityDAO {
-    
+
+    @Insert
+    fun save(list: List<PriorityModel>)
 }
