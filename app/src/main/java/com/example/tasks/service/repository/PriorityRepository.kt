d2d0e6f13438ac41eceaa9aa6f0b335a9b56c1a5
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-30 14:57:41 -0300

    Add service and implement members

--- app/src/main/java/com/example/tasks/service/repository/PriorityRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PriorityRepository.kt
@@ -1,5 +1,29 @@
 package com.example.tasks.service.repository
 
+import com.example.tasks.service.model.PriorityModel
+import com.example.tasks.service.repository.remote.PriorityService
+import com.example.tasks.service.repository.remote.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class PriorityRepository {
-    
+    private val mRemote = RetrofitClient.createService(PriorityService::class.java)
+
+    fun all() {
+        val call: Call<List<PriorityModel>> = mRemote.list()
+        call.enqueue(object : Callback<List<PriorityModel>> {
+            override fun onFailure(call: Call<List<PriorityModel>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onResponse(
+                call: Call<List<PriorityModel>>,
+                response: Response<List<PriorityModel>>
+            ) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
 }
