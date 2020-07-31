41272aa32507dbc20f8858fac0650498897aa46a
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-31 13:35:18 -0300

    Add listPriorities function

--- app/src/main/java/com/example/tasks/viewmodel/TaskFormViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/TaskFormViewModel.kt
@@ -2,9 +2,13 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.tasks.service.repository.PriorityRepository
 
 class TaskFormViewModel(application: Application) : AndroidViewModel(application) {
 
-    
+    private val mPriorityRepository = PriorityRepository(application)
 
+    fun listPriorities() {
+        mPriorityRepository.all()
+    }
 }
