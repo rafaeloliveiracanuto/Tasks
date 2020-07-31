2b61d3bf19abf8b2d26f4191faaaaa158d0501c8
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-31 14:08:56 -0300

    Create save function

--- app/src/main/java/com/example/tasks/viewmodel/TaskFormViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/TaskFormViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.tasks.service.model.PriorityModel
+import com.example.tasks.service.model.TaskModel
 import com.example.tasks.service.repository.PriorityRepository
 
 class TaskFormViewModel(application: Application) : AndroidViewModel(application) {
@@ -16,5 +17,9 @@ class TaskFormViewModel(application: Application) : AndroidViewModel(application
 
     fun listPriorities() {
         mPriorityList.value = mPriorityRepository.list()
+    }
+
+    fun save(task: TaskModel) {
+
     }
 }
