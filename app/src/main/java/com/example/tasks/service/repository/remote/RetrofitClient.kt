a823401018b246ad08cda2de6a485c0619ea9f4d
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-07-31 23:00:23 -0300

    Add interceptor

--- app/src/main/java/com/example/tasks/service/repository/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/tasks/service/repository/remote/RetrofitClient.kt
@@ -1,6 +1,9 @@
 package com.example.tasks.service.repository.remote
 
+import com.example.tasks.service.constants.TaskConstants
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -10,10 +13,26 @@ class RetrofitClient private constructor(){
 
         private lateinit var retrofit: Retrofit
         private const val BASE_URL = "http://devmasterteam.com/CursoAndroidAPI/"
+        private var personKey = ""
+        private var tokenKey = ""
 
         private fun getRetrofitInstance() : Retrofit {
-
             val httpClient = OkHttpClient.Builder()
+            httpClient.addInterceptor(object : Interceptor {
+                override fun intercept(chain: Interceptor.Chain): Response {
+                    val request =
+                        chain.request()
+                            .newBuilder()
+                            .addHeader(TaskConstants.HEADER.PERSON_KEY, personKey)
+                            .addHeader(TaskConstants.HEADER.TOKEN_KEY, tokenKey)
+                            .build()
+                    return chain.proceed(request)
+                }
+
+            }
+
+            )
+
             if (!Companion::retrofit.isInitialized) {
                 retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL)
