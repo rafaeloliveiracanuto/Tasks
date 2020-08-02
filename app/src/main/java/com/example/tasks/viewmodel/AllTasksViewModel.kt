82d9f5ec4a8a21612941cc6d372e49ddcef597c7
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-01 23:39:10 -0300

    Create list function

--- app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
@@ -5,8 +5,14 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.tasks.service.repository.TaskRepository
 
 class AllTasksViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val mTaskRepository = TaskRepository(application)
+
+    fun list() {
+        
+    }
 
 }
