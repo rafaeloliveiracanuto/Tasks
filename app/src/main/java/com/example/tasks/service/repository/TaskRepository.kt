5447e363bbd144076b9ecb994b54987bb4e7b0b4
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-01 23:33:22 -0300

    Add nextWeek function

--- app/src/main/java/com/example/tasks/service/repository/TaskRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/TaskRepository.kt
@@ -16,6 +16,16 @@ class TaskRepository(val context: Context ) {
 
     private val mRemote = RetrofitClient.createService(TaskService::class.java)
 
+    fun all(listener: APIListener<List<TaskModel>>) {
+        val call: Call<List<TaskModel>> = mRemote.all()
+        list(call, listener)
+    }
+
+    fun nextWeek(listener: APIListener<List<TaskModel>>) {
+        val call: Call<List<TaskModel>> = mRemote.nextWeek()
+        list(call, listener)
+    }
+
     private fun list(call: Call<List<TaskModel>>, listener: APIListener<List<TaskModel>>) {
         call.enqueue(object : Callback<List<TaskModel>> {
             override fun onFailure(call: Call<List<TaskModel>>, t: Throwable) {
@@ -35,29 +45,6 @@ class TaskRepository(val context: Context ) {
 
         })
     }
-
-    fun all(listener: APIListener<List<TaskModel>>) {
-        val call: Call<List<TaskModel>> = mRemote.all()
-        call.enqueue(object : Callback<List<TaskModel>> {
-            override fun onFailure(call: Call<List<TaskModel>>, t: Throwable) {
-                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
-            }
-
-            override fun onResponse(call: Call<List<TaskModel>>, response: Response<List<TaskModel>>) {
-                if (response.code() != TaskConstants.HTTP.SUCCESS) {
-                    val validation = Gson().fromJson(response.errorBody()!!.string(), String::class.java)
-                    listener.onFailure(validation)
-                } else {
-                    response.body()?.let {
-                        listener.onSuccess(it)
-                    }
-                }
-            }
-
-        })
-    }
-
-    
 
     fun create(task: TaskModel, listener: APIListener<Boolean>) {
         val call: Call<Boolean> =
