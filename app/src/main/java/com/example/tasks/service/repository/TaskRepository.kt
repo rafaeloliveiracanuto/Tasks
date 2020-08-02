af2eebb381c7c51122728f0ae01e66c91cffd618
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-01 22:29:56 -0300

    Create create function

--- app/src/main/java/com/example/tasks/service/repository/TaskRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/TaskRepository.kt
@@ -1,4 +1,14 @@
 package com.example.tasks.service.repository
 
+import com.example.tasks.service.model.TaskModel
+import com.example.tasks.service.repository.remote.RetrofitClient
+import com.example.tasks.service.repository.remote.TaskService
+
 class TaskRepository {
+
+    private val mRemote = RetrofitClient.createService(TaskService::class.java)
+
+    fun create(task: TaskModel) {
+        mRemote.create()
+    }
 }
