2cf40ed0c67f9d3b429fced65465fc3ce4bd44f3
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-02 10:21:18 -0300

    Fill

--- app/src/main/java/com/example/tasks/view/viewholder/TaskViewHolder.kt
+++ app/src/main/java/com/example/tasks/view/viewholder/TaskViewHolder.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.tasks.R
 import com.example.tasks.service.listener.TaskListener
 import com.example.tasks.service.model.TaskModel
+import com.example.tasks.service.repository.PriorityRepository
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -16,6 +17,8 @@ class TaskViewHolder(itemView: View, val listener: TaskListener) :
     RecyclerView.ViewHolder(itemView) {
 
     private val mDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+    private val mPriorityRepository = PriorityRepository(itemView.context)
+
     private var mTextDescription: TextView = itemView.findViewById(R.id.text_description)
     private var mTextPriority: TextView = itemView.findViewById(R.id.text_priority)
     private var mTextDueDate: TextView = itemView.findViewById(R.id.text_due_date)
@@ -24,8 +27,8 @@ class TaskViewHolder(itemView: View, val listener: TaskListener) :
     fun bindData(task: TaskModel) {
 
         this.mTextDescription.text = task.description
-        this.mTextPriority.text = ""
-        this.mTextDueDate.text = ""
+        
+        this.mTextPriority.text = mPriorityRepository.getDescription(task.priorityId)
 
         val date = SimpleDateFormat("yyyy-MM-dd").parse(task.dueDate)
         this.mTextDueDate.text = mDateFormat.format(date)
