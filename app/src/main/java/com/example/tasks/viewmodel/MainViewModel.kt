be1ff8598b62e386a2cedf681d1a3932a3db80b4
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-04 13:39:52 -0300

    Create loadUserName function

--- app/src/main/java/com/example/tasks/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/MainViewModel.kt
@@ -4,4 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+    fun loadUserName() {
+        
+    }
 }
