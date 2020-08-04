a49a2ed523afd2b1b8bd603e1576ed78a049a1ca
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-03 22:47:39 -0300

    Implement isConnectionAvailable

--- app/src/main/java/com/example/tasks/service/repository/PriorityRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PriorityRepository.kt
@@ -1,6 +1,10 @@
 package com.example.tasks.service.repository
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.model.PriorityModel
 import com.example.tasks.service.repository.local.TaskDatabase
@@ -38,6 +42,31 @@ class PriorityRepository(context: Context) {
     fun getDescription(id: Int) = mPriorityDatabase.getDescription(id)
 
     fun isConnectionAvailable(context: Context): Boolean {
-        
+
+        var result = false
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = cm.activeNetwork ?: return false
+            val actNw = cm.getNetworkCapabilities(networkCapabilities) ?: return false
+
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            cm.run {
+                cm.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        else -> false
+                    }
+                }
+            }
+        }
+
+        return result
     }
 }
