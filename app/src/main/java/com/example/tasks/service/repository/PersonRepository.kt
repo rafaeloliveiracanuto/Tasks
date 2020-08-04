1c92f016eb3f99803a13cfb5a57576843d47d144
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-03 22:59:44 -0300

    Add internet checking in login

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,6 +1,7 @@
 package com.example.tasks.service.repository
 
 import android.content.Context
+import android.provider.Settings.Global.getString
 import com.example.tasks.R
 import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
@@ -12,11 +13,17 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PersonRepository(val context: Context){
+class PersonRepository(val context: Context): BaseRepository(context){
 
     private val mRemote = RetrofitClient.createService(PersonService::class.java)
 
     fun login(email: String, password: String, listener: APIListener<HeaderModel>) {
+
+        if (!isConnectionAvailable(context)) {
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
+
         val call: Call<HeaderModel> = mRemote.login(email, password)
         call.enqueue(object : Callback<HeaderModel> {
             override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
