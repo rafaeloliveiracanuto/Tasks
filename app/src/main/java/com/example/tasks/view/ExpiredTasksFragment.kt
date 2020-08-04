f04ee7b8f776735f717bebb2e9fe380724a3ff90
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-04 13:05:38 -0300

    Delete viewModels

--- app/src/main/java/com/example/tasks/view/ExpiredTasksFragment.kt
+++ app/src/main/java/com/example/tasks/view/ExpiredTasksFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.tasks.R
-import com.example.tasks.viewmodel.ExpiredTasksViewModel
 
 class ExpiredTasksFragment : Fragment() {
 
