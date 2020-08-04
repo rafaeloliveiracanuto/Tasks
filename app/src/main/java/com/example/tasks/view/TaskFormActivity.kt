056b6da7825ce6c21e58950d5597c450e5157fc6
Rafael Oliveira Canuto <rafaeloliveiracanuto@gmail.com>
2020-08-03 22:08:27 -0300

    Add finish in observe

--- app/src/main/java/com/example/tasks/view/TaskFormActivity.kt
+++ app/src/main/java/com/example/tasks/view/TaskFormActivity.kt
@@ -100,6 +100,7 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener, DatePickerDi
         mViewModel.validation.observe(this, androidx.lifecycle.Observer {
             if (it.success()) {
                 Toast.makeText(this, "Sucesso!", Toast.LENGTH_SHORT).show()
+                finish()
             } else {
                 Toast.makeText(this, it.failure(), Toast.LENGTH_SHORT).show()
             }
